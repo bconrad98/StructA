e6653bc5906c97fbf70fbabda63fc3fe3fe2c71e
Braeden Conrad <bconrad98@gmail.com>
2019-03-03 13:24:11 -0600

    Added documentation to main.py

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ import useful_functions as uf
 import argparse
 
 def main():
+	# Add argument parser for easy command line use
 	parser = argparse.ArgumentParser(description="Main file for solving truss structure")
-	parser.add_argument('folder',help='The path to the directory containing displacements,elements,forces,nodes files')
+	parser.add_argument('folder_path',help='The path to the directory containing displacements,elements,forces,nodes files')
 	args = parser.parse_args()
-	nodes = uf.create_nodes(args.folder+"/nodes.txt",
-						 args.folder+"/displacements.txt",
-						 args.folder+"/forces.txt")
-	eles = uf.create_eles(args.folder+"/elements.txt",nodes)
+	# get list of all the nodes from input files
+	nodes = uf.create_nodes(args.folder_path+"/nodes.txt",
+						 args.folder_path+"/displacements.txt",
+						 args.folder_path+"/forces.txt")
+	# get list of all elements from input files
+	eles = uf.create_eles(args.folder_path+"/elements.txt",nodes)
+	# get the truss object
 	truss = ts.TrussSolver(eles,nodes)
+	# solve for unkown displacements
 	u_sol = truss.solve()
+	# print out all displacements in a pretty way
 	print("All displacements in order:")
 	node_num = 0
 	for node in truss.nodes:
